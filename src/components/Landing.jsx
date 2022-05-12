a206da487a84920c716f359a777824991f4b0b05
Becca-Guertin <becca_guertin@hotmail.com>
2022-05-11 20:59:44 -0700

    fixed size of right multi colored flower icon

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,22 +1,17 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Grid, Card, CardContent } from "@mui/material";
+import { Grid } from "@mui/material";
 import SimpleImageSlider from "react-simple-image-slider";
 import Modal1 from "../components/Modals/Modal1";
 import helloText from "../assets/images/HelloWorldIMG.png";
 import greenBG from "../assets/images/GreenBannerIMG.png";
 import bioIMG from "../assets/images/BeccaBioIMG.png";
-// import img1 from "../assets/images/SunPopShop logo.png";
 import img2 from "../assets/images/ClownBabeTexture-min.png";
 import img3 from "../assets/images/PlantAlienBabeTex_colors.png";
-// import img4 from "../assets/images/WipHHpoisonVialAmpBgTex (1).png";
 import img5 from "../assets/images/EverythingMeansNothing.PNG";
 import img6 from "../assets/images/Modern-Pulp-celebrateNewChallenges.PNG";
-// import img7 from "../assets/images/Bending backwards_NoBckg2.jpg";
 import img8 from "../assets/images/DE0A4174-6922-4D78-97C8-D40DB7961115.JPG";
 import img9 from "../assets/images/SurprisedAlienBabe_redTex.png";
-// import img10 from "../assets/images/PleatherGalaxyBabe_tex.png";
-// import img11 from "../assets/images/PoisonHHampNoBG1.png";
 import img12 from "../assets/images/Modern-Pulp-myHeartRevamp.PNG";
 import img13 from "../assets/images/TransparentBannerCrop_h&h.png";
 import img14 from "../assets/images/Sunpoplogostamp.png";
@@ -33,7 +28,6 @@ const Landing = () => {
 
   const handleClick = () => {
     setIsModalOpen2(true);
-    // alert("Press Esc to return to About");
   };
 
   return (
@@ -101,7 +95,7 @@ const Landing = () => {
               alignItems="center"
             >
               <Grid item>
-                <Link to="" title="click for more about me!">
+                <Link to="" title="">
                   <img
                     src={bioIMG}
                     alt="becca is an artist and software engineer based out of the greater seattle area"
@@ -191,8 +185,8 @@ const Landing = () => {
                   alt="multi-colored flower icon"
                   src="https://sunpopbucket.s3.us-west-1.amazonaws.com/images/MultiColorFlowerStamp1.png"
                   style={{
-                    maxHeight: 300,
-                    maxWidth: 300,
+                    maxHeight: 175,
+                    maxWidth: 175,
                     padding: 75,
                   }}
                 />
@@ -216,7 +210,7 @@ const Landing = () => {
               alignItems="center"
               spacing={2}
             >
-              <Grid item xs={3}>
+              <Grid item xs={4}>
                 <a
                   title="Hazards and Hijinks"
                   href="https://dev.d1wehpq1tr8ee4.amplifyapp.com/"
@@ -235,7 +229,7 @@ const Landing = () => {
                 </a>
                 {/* <tt style={{ fontSize: 30 }}>Hazards and Hijinks</tt> */}
               </Grid>
-              <Grid item xs={3}>
+              <Grid item xs={4}>
                 <a title="sunpopshop.com" href="https://sunpopshop.com">
                   <img
                     src={img14}
@@ -251,7 +245,7 @@ const Landing = () => {
                 </a>
                 {/* <tt style={{ fontSize: 30 }}>sunpopshop.com</tt> */}
               </Grid>
-              <Grid item xs={3}>
+              <Grid item xs={4}>
                 <a
                   title="Host a Fan"
                   href="https://hostafan.azurewebsites.net/"
